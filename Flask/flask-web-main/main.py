51658907227ce9cc053a56aabbe8a6530ea2b06b
Nijiiro <61643144+boom141@users.noreply.github.com>
2022-10-10 17:17:02 +0800

    Update main.py

--- Flask/flask-web-main/main.py
+++ Flask/flask-web-main/main.py
@@ -11,26 +11,28 @@ def test():
 
 @app.route("/send_mail")
 def send_mail():
-mail_content = 'hellos this is a sample mail'
-#The mail addresses and password
-sender_address = 'sender123@gmail.com'
-sender_pass = 'xxxxxxxx'
-receiver_address = 'receiver567@gmail.com'
-#Setup the MIME
-message = MIMEMultipart()
-message['From'] = sender_address
-message['To'] = receiver_address
-message['Subject'] = 'A test mail sent by Python. It has an attachment.'   #The subject line
-#The body and the attachments for the mail
-message.attach(MIMEText(mail_content, 'plain'))
-#Create SMTP session for sending the mail
-session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
-session.starttls() #enable security
-session.login(sender_address, sender_pass) #login with mail_id and password
-text = message.as_string()
-session.sendmail(sender_address, receiver_address, text)
-session.quit()
-print('Mail Sent')
+    mail_content = 'hellos this is a sample mail'
+    #The mail addresses and password
+    sender_address = 'sender123@gmail.com'
+    sender_pass = 'xxxxxxxx'
+    receiver_address = 'receiver567@gmail.com'
+    #Setup the MIME
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = 'A test mail sent by Python. It has an attachment.'   #The subject line
+    #The body and the attachments for the mail
+    message.attach(MIMEText(mail_content, 'plain'))
+    #Create SMTP session for sending the mail
+    session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+    session.starttls() #enable security
+    session.login(sender_address, sender_pass) #login with mail_id and password
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
+    print('Mail Sent')
+
+     return redirect(url_for("Test"))
 
 if __name__ == "__main__":
     app.run(debug=True)
